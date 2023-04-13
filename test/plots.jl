4cf5c6488d112a85ebbf94cb95aa81944c3ec5ec
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-04-13 14:50:25 +0200

    Add Makie recipes

---
+++ test/plots.jl
@@ -0,0 +1,35 @@
+using CairoMakie
+using VortexFilamentEwald.Filaments
+using Test
+
+function init_trefoil_filament(N::Int)
+    S(t) = (
+        sinpi(t) + 2 * sinpi(2t),
+        cospi(t) - 2 * cospi(2t),
+        -sinpi(3t),
+    )
+    f = Filaments.init(ClosedFilament, N, CubicSplineMethod())
+    ζs = range(0, 2; length = N + 1)[1:N]
+    f .= S.(ζs)
+    update_coefficients!(f)
+    f
+end
+
+@testset "Makie recipes" begin
+    # Just make sure that things don't error.
+    @testset "Filaments" begin
+        f = init_trefoil_filament(16)
+        fig = Figure()
+        ax = Axis3(fig[1, 1])
+        filamentplot!(
+            f;
+            refinement = 8,
+            color = :Grey,
+            tangents = true, tangentcolor = :Blue,
+            curvatures = true, curvaturecolor = :Red,
+            vectorpos = 0.3,
+        )
+        save("trefoil.png", fig)
+        @test isfile("trefoil.png")
+    end
+end
