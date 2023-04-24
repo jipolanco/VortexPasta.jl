ef252909ffec72d9bbdd58f01f70ad302d8206ab
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-04-24 16:32:43 +0200

    Makie: allow plotting filament velocities

--- src/Filaments/makie_recipes.jl
+++ src/Filaments/makie_recipes.jl
@@ -4,8 +4,8 @@ using LinearAlgebra: normalize
 export filamentplot, filamentplot!
 
 """
-    filamentplot(f::AbstractFilament; kws...)
-    MakieCore.plot(f::AbstractFilament; kws...)
+    filamentplot(f::AbstractFilament, [velocities]; kws...)
+    MakieCore.plot(f::AbstractFilament, [velocities]; kws...)
 
 Plot a filament using Makie.jl.
 
@@ -21,8 +21,8 @@ See [`filamentplot!`](@ref) for details and for optional keyword arguments.
 function filamentplot end
 
 """
-    filamentplot!([ax,] f::AbstractFilament; kws...)
-    MakieCore.plot!(ax, f::AbstractFilament; kws...)
+    filamentplot!([ax,] f::AbstractFilament, [velocities]; kws...)
+    MakieCore.plot!(ax, f::AbstractFilament, [velocities]; kws...)
 
 Plot filament onto existent 3D axis.
 
@@ -51,6 +51,10 @@ plot!(ax, f)  # f is a filament
 
 - `markersize = 10.0f0`
 
+- `arrowscale = 1.0f0` allows to scale vectors (tangents, curvatures, velocities, …)
+
+- `colormap = :viridis`
+
 ### Plotting tangent and curvature vectors
 
 Tangent and curvature vectors can be optionally plotted via the `tangents` and
@@ -68,52 +72,83 @@ them in-between nodes using the `vectorpos` argument.
 - `curvaturecolor = nothing`
 
 - `vectorpos = 0.0`: relative vector positions within each segment. Must be in ``[0, 1]``.
+
+### Plotting velocities of filament nodes
+
+Similarly, it is possible to plot vector quantities attached to filament nodes,
+such as filament velocities. For this pass a vector of velocities as a
+positional argument after the filament `f`.
+
+Associated keyword arguments:
+
+- `velocitycolor = nothing` colour of velocity vectors
+
 """
 function filamentplot! end
 
-MakieCore.plot(f::AbstractFilament; kws...) = filamentplot(f; kws...)
-MakieCore.plot!(ax, f::AbstractFilament; kws...) = filamentplot!(ax, f; kws...)
+MakieCore.plot(f::AbstractFilament, args...; kws...) = filamentplot(f, args...; kws...)
+MakieCore.plot!(ax, f::AbstractFilament, args...; kws...) = filamentplot!(ax, f, args...; kws...)
 
 # This macro defines the functions `filamentplot` and `filamentplot!` and the type FilamentPlot,
 # among other things.
-MakieCore.@recipe(FilamentPlot, filament) do scene
+MakieCore.@recipe(FilamentPlot) do scene
     MakieCore.Attributes(
         refinement = 1,
         marker = :circle,
         markersize = 10.0f0,
         linewidth = 1.5f0,
         color = :black,
+        colormap = :viridis,
         markercolor = nothing,  # defaults to `color`
+        arrowscale = 1.0f0,
         tangents = false,
         curvatures = false,
         tangentcolor = nothing,
         curvaturecolor = nothing,
         vectorpos = 0.0,
+        velocitycolor = nothing,
     )
 end
 
+const PlotFilamentOnly = FilamentPlot{<:Tuple{<:AbstractFilament}}
+const PlotFilamentAndVelocities = FilamentPlot{<:Tuple{<:AbstractFilament, <:AbstractVector{<:Vec3}}}
+
+# For some reason the second argument (`len`) is also needed in dispatch (not really mentioned in the Makie docs...).
+MakieCore.argument_names(::Type{<:PlotFilamentOnly}, len::Integer) = (:filament,)
+MakieCore.argument_names(::Type{<:PlotFilamentAndVelocities}, len::Integer) = (:filament, :velocities)
+
+fixfirst(f::F, x) where {F} = (ys...) -> f(x, ys...)
+
 # We use the fact that `Makie.lift` (not included in MakieCore) is the same as `map`.
 function MakieCore.plot!(p::FilamentPlot)
+    argnames = MakieCore.argument_names(p)
     f = p.filament
-    f[] isa AbstractFilament || error("expected an AbstractFilament as first argument")
+    v = (:velocities ∈ argnames) ? p.velocities : nothing
     Xs_nodes = map(_select_points_to_plot, f)
     Xs_line = map(_refine_filament, f, p.refinement)
     MakieCore.lines!(
         p, Xs_line;
         color = p.color, linewidth = p.linewidth,
+        colormap = p.colormap,
     )
     MakieCore.scatter!(
         p, Xs_nodes;
         color = map(something, p.markercolor, p.color),
         marker = p.marker, markersize = p.markersize,
+        colormap = p.colormap,
     )
-    tangentcolor = map(something, p.tangentcolor, p.color)
-    map(f, p.tangents, p.vectorpos, tangentcolor) do (args...)
-        _plot_tangents!(p, args...)
+    arrowscale = p.arrowscale
+    let
+        tangentcolor = map(something, p.tangentcolor, p.color)
+        map(fixfirst(_plot_tangents!, p), f, p.tangents, p.vectorpos, tangentcolor, p.colormap, arrowscale)
     end
-    curvaturecolor = map(something, p.curvaturecolor, p.color)
-    map(f, p.curvatures, p.vectorpos, curvaturecolor) do (args...)
-        _plot_curvatures!(p, args...)
+    let
+        curvaturecolor = map(something, p.curvaturecolor, p.color,)
+        map(fixfirst(_plot_curvatures!, p), f, p.curvatures, p.vectorpos, curvaturecolor, p.colormap, arrowscale)
+    end
+    if v !== nothing
+        velocitycolor = map(something, p.velocitycolor, p.color,)
+        map(fixfirst(_plot_velocities!, p), f, v, velocitycolor, p.colormap, arrowscale)
     end
     p
 end
@@ -123,8 +158,7 @@ _select_points_to_plot(f::ClosedFilament) = nodes(f)[begin:end + 1]
 
 function _plot_tangents!(
         p::FilamentPlot, f::ClosedFilament, tangents::Bool,
-        vectorpos::Real,
-        tangentcolor,
+        vectorpos::Real, tangentcolor, colormap, arrowscale::Real,
     )
     tangents || return p
     N = length(f)
@@ -141,7 +175,8 @@ function _plot_tangents!(
     MakieCore.arrows!(
         p, Xs..., Vs...;
         color = tangentcolor,
-        _arrow_kwargs()...,
+        colormap,
+        _arrow_kwargs(; arrowscale,)...,
     )
     p
 end
@@ -150,6 +185,8 @@ function _plot_curvatures!(
         p::FilamentPlot, f::ClosedFilament, curvatures::Bool,
         vectorpos::Real,
         curvaturecolor,
+        colormap,
+        arrowscale::Real,
     )
     curvatures || return p
     N = length(f)
@@ -169,14 +206,38 @@ function _plot_curvatures!(
     MakieCore.arrows!(
         p, Xs..., Vs...;
         color = curvaturecolor,
-        _arrow_kwargs()...,
+        colormap,
+        _arrow_kwargs(; arrowscale,)...,
     )
     p
 end
 
-_arrow_kwargs() = (;
+function _plot_velocities!(
+        p::FilamentPlot, f::AbstractFilament, v::AbstractVector, velocitycolor,
+        colormap,
+        arrowscale::Real,
+    )
+    length(v) == length(f) || throw(DimensionMismatch("wront length of velocity vector"))
+    N = length(f)
+    Xs = ntuple(_ -> Vector{Float32}(undef, N), 3)  # layout accepted by Makie.arrows
+    Vs = map(similar, Xs)
+    for i ∈ eachindex(f), n ∈ eachindex(Xs)
+        Xs[n][i] = f[i][n]
+        Vs[n][i] = v[i][n]
+    end
+    MakieCore.arrows!(
+        p, Xs..., Vs...;
+        color = velocitycolor,
+        colormap,
+        _arrow_kwargs(; arrowscale,)...,
+    )
+    p
+end
+
+_arrow_kwargs(; arrowscale,) = (;
     arrowsize = (0.15, 0.15, 0.25),
     linewidth = 0.04,
+    lengthscale = arrowscale,
 )
 
 function _refine_filament(f::ClosedFilament, refinement::Int)
--- test/plots.jl
+++ test/plots.jl
@@ -1,5 +1,8 @@
 using CairoMakie
+using LinearAlgebra: norm
+using Random
 using VortexPasta.Filaments
+using VortexPasta.Filaments: Vec3
 using Test
 
 function init_trefoil_filament(N::Int)
@@ -17,24 +20,41 @@ end
 
 @testset "Makie recipes" begin
     # Just make sure that things don't error.
+    Random.seed!(42)
     @testset "Filaments" begin
         f = init_trefoil_filament(16)
-        fig = Figure()
-        ax = Axis3(fig[1, 1])
-        plot!(  # same as `filamentplot!`
-            ax, f;
-            refinement = 8,
-            color = :Grey,
-            tangents = true, tangentcolor = :Blue,
-            curvatures = true, curvaturecolor = :Red,
-            vectorpos = 0.3,
-        )
-        save("trefoil.png", fig)
-        @test isfile("trefoil.png")
-        let
+        @testset "Trefoil basic" begin
             plt = plot(f; axis = (type = Axis3,))  # same as `filamentplot`
             save("trefoil_basic.png", plt)
             @test isfile("trefoil_basic.png")
+        end
+        @testset "Trefoil" begin
+            fig = Figure()
+            ax = Axis3(fig[1, 1])
+            plot!(  # same as `filamentplot!`
+                ax, f;
+                refinement = 8,
+                color = :Grey,
+                tangents = true, tangentcolor = :Blue,
+                curvatures = true, curvaturecolor = :Red,
+                vectorpos = 0.3,
+            )
+            save("trefoil.png", fig)
+            @test isfile("trefoil.png")
+        end
+        @testset "Trefoil with velocities" begin
+            velocities = randn(Vec3{Float32}, length(nodes(f)))
+            fig = Figure()
+            ax = Axis3(fig[1, 1]; aspect = :data)
+            plot!(  # same as `filamentplot!`
+                ax, f, velocities;
+                refinement = 4,
+                color = :Grey,
+                velocitycolor = norm.(velocities),
+                colormap = :cividis,
+            )
+            save("trefoil_velocities.png", fig)
+            @test isfile("trefoil_velocities.png")
         end
     end
 end
