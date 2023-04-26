052d37609e3282cc71cc7e6c689bd509539a7891
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-04-26 16:56:34 +0200

    Enable ring collision test (!)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using VortexPasta
 @testset "VortexPasta.jl" begin
     include("filaments.jl")
     include("ring.jl")
+    include("ring_collision.jl")
     include("trefoil.jl")
     include("plots.jl")
 end
