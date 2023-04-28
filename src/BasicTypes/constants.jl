a9bc119c22bd06cf51770187a731339e457c4922
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-04-28 16:54:10 +0200

    Remove unused definitions

--- src/BasicTypes/constants.jl
+++ src/BasicTypes/constants.jl
@@ -9,12 +9,9 @@ struct Zero <: Real end
 
 (::Zero) * (::Number) = Zero()
 (::Number) * (::Zero) = Zero()
-(::Zero) * (::Zero) = Zero()
-(::Zero) + (x::Number) = x
 (x::Number) + (::Zero) = x
 (::Zero) + (::Zero) = Zero()
 (::Zero) / (::Number) = Zero()
-(::Zero)^(::Integer) = Zero()
 
 """
     Infinity <: Real
@@ -31,14 +28,11 @@ Alias for Infinity().
 const ∞ = Infinity()
 
 # Here we assume that Number > 0, which is the case in our application.
-(::Infinity) * (::Number) = Infinity()
 (::Number) * (::Infinity) = Infinity()
 (::Infinity) * (::Infinity) = Infinity()
 (::Infinity) / (::Number) = Infinity()
 
-(::Number) < (::Infinity) = true
 (::Infinity) < (::Real) = false   # needs to be Real to disambiguate
 (::Infinity) < (::Infinity) = false
 
-(::Number) / (::Infinity) = Zero()
 (::Number) / (::Zero) = Infinity()
