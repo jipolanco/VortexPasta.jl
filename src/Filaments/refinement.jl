fb53910132c6e9266c166e56833ee61070f5d429
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-05-05 16:34:23 +0200

    Refinement: add ℓ_max option

--- src/Filaments/refinement.jl
+++ src/Filaments/refinement.jl
@@ -24,7 +24,7 @@ function refine! end
 
 """
     BasedOnCurvature <: RefinementCriterion
-    BasedOnCurvature(ρℓ_max::Real, ρℓ_min = ρℓ_max / 2)
+    BasedOnCurvature(ρℓ_max::Real, ρℓ_min = ρℓ_max / 2; ℓ_max = Inf)
 
 Curvature-based refinement criterion.
 
@@ -37,19 +37,24 @@ According to this criterion, a filament is locally refined if:
 where ``ρ_{i + 1/2}`` is the curvature in the middle of the segment.
 
 Similarly, filaments nodes are removed based on the value of `ρℓ_min`.
+Note that, when filaments are nearly straight, this may lead to the removal of most nodes.
+To limit this, set the keyword argument `ℓ_max` to some finite value
+determining the maximum length of a segment.
+This setting will be roughly respected when removing nodes.
 """
 struct BasedOnCurvature <: RefinementCriterion
     ρℓ_max :: Float64
     ρℓ_min :: Float64
+    ℓ_max  :: Float64
     inds   :: Vector{Int}   # indices of nodes or segments to modify
     remove :: Vector{Bool}  # determine whether nodes should be removed or segments should be refined
-    BasedOnCurvature(ρℓ_max, ρℓ_min) = new(ρℓ_max, ρℓ_min, Int[], Bool[])
+    BasedOnCurvature(ρℓ_max, ρℓ_min; ℓ_max = Inf) = new(ρℓ_max, ρℓ_min, ℓ_max, Int[], Bool[])
 end
 
-BasedOnCurvature(ρℓ_max) = BasedOnCurvature(ρℓ_max, ρℓ_max / 2.1)
+BasedOnCurvature(ρℓ_max; kws...) = BasedOnCurvature(ρℓ_max, ρℓ_max / 2.5; kws...)
 
 function _nodes_to_refine!(f::AbstractFilament, crit::BasedOnCurvature)
-    (; ρℓ_max, ρℓ_min, inds, remove,) = crit
+    (; ρℓ_max, ρℓ_min, ℓ_max, inds, remove,) = crit
     ts = knots(f)
     n_add = n_rem = 0
     empty!(inds)
@@ -68,7 +73,7 @@ function _nodes_to_refine!(f::AbstractFilament, crit::BasedOnCurvature)
             push!(inds, i)
             push!(remove, false)
             n_add += 1
-        elseif ρℓ < ρℓ_min
+        elseif ρℓ < ρℓ_min && ℓ < ℓ_max / 2  # so that the new ℓ is roughly smaller than ℓ_max
             push!(inds, i + 1)
             push!(remove, true)
             n_rem += 1
