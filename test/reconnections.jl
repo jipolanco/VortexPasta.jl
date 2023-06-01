876dad7936fb1551c00dd3cb390e66658a70e5f4
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-06-01 12:56:03 +0200

    Add trefoil knot self-reconnection test

--- test/reconnections.jl
+++ test/reconnections.jl
@@ -1,5 +1,7 @@
 using Test
 using VortexPasta.Filaments
+using VortexPasta.BiotSavart
+using VortexPasta.Timestepping
 
 # Create a curve which resembles an 8 (or ∞).
 # This specific curve is called the lemniscate of Bernouilli (https://en.wikipedia.org/wiki/Lemniscate_of_Bernoulli)
@@ -17,43 +19,109 @@ function figure_eight_curve(; a::Real = 1, origin::Vec3 = π * Vec3(1, 1, 1), Az
     (; tlims, S, a, c, Az, origin,)
 end
 
-@testset "Reconnections: ∞ curve" begin
-    Az = 0.001
-    curve = figure_eight_curve(; a = π / 4, Az, origin = Vec3(0, 0, 0))
-    (; S, tlims,) = curve
-    N = 32
-    ζs = range(tlims...; length = 2N + 1)[2:2:2N]
-    f = Filaments.init(ClosedFilament, S.(ζs), CubicSplineMethod())
+function trefoil_knot_curve(; a::Real, origin::Vec3 = π * Vec3(1, 1, 1))
+    tlims = (-1, 1)
+    S(t) = origin + a * Vec3(
+        sinpi(t) + 2 * sinpi(2t),
+        cospi(t) - 2 * cospi(2t),
+        -sinpi(3t),
+    )
+    (; tlims, S, a, origin,)
+end
 
-    ts = knots(f)
-    l_min = minimum(eachindex(ts)) do i
-        ts[i + 1] - ts[i]
+@testset "Reconnections" begin
+    @testset "Static: figure-eight knot" begin
+        Az = 0.001
+        curve = figure_eight_curve(; a = π / 4, Az, origin = Vec3(0, 0, 0))
+        (; S, tlims,) = curve
+        N = 32
+        ζs = range(tlims...; length = 2N + 1)[2:2:2N]
+        f = Filaments.init(ClosedFilament, S.(ζs), CubicSplineMethod())
+
+        l_min = minimum_knot_increment(f)
+
+        @testset "Filaments.split!" begin
+            i = length(f) ÷ 4
+            j = 3i
+            f1, f2 = @inferred Filaments.split!(copy(f), i, j)
+            update_coefficients!.((f1, f2))
+            @test length(f1) == length(f2) == length(f) ÷ 2
+            @test f1 == f[i + 1:j]
+            @test f2 == vcat(f[begin:i], f[j + 1:end])
+        end
+
+        @testset "reconnect_self!" begin
+            crit = @inferred BasedOnDistance(l_min / 2)
+            fc = copy(f)
+            fs_all = [fc]
+            Filaments.reconnect_self!(crit, fc, fs_all)
+            @test length(fs_all) == 2  # filament split into two!
+            f1, f2 = fs_all[1], fs_all[2]
+
+            # Check that the reconnection happened at the right place
+            i = length(f) ÷ 4
+            j = 3i
+            @test length(f1) == length(f2) == length(f) ÷ 2
+            @test f1 == f[i + 1:j]
+            @test f2 == vcat(f[begin:i], f[j + 1:end])
+        end
     end
 
-    @testset "Filaments.split!" begin
-        i = length(f) ÷ 4
-        j = 3i
-        f1, f2 = @inferred Filaments.split!(copy(f), i, j)
-        update_coefficients!.((f1, f2))
-        @test length(f1) == length(f2) == length(f) ÷ 2
-        @test f1 == f[i + 1:j]
-        @test f2 == vcat(f[begin:i], f[j + 1:end])
-    end
+    @testset "Dynamic: trefoil knot" begin
+        curve = trefoil_knot_curve(; a = π / 4,)
+        (; S, tlims,) = curve
+        N = 64
+        ζs = range(tlims...; length = 2N + 1)[2:2:2N]
+        f = Filaments.init(ClosedFilament, S.(ζs), CubicSplineMethod())
+        fs = [f]
+        l_min = minimum_knot_increment(fs)
 
-    @testset "reconnect_self!" begin
-        crit = @inferred BasedOnDistance(l_min / 2)
-        fc = copy(f)
-        fs_all = [fc]
-        Filaments.reconnect_self!(crit, fc, fs_all)
-        @test length(fs_all) == 2  # filament split into two!
-        f1, f2 = fs_all[1], fs_all[2]
+        params_bs = let
+            Ls = (1, 1, 1) .* 2π
+            Ns = (1, 1, 1) .* 32
+            kmax = (Ns[1] ÷ 2) * 2π / Ls[1]
+            α = kmax / 5
+            rcut = 4 / α
+            ParamsBiotSavart(;
+                Γ = 2.0, α, a = 1e-6, Δ = 1/4, rcut, Ls, Ns,
+                backend_short = NaiveShortRangeBackend(),
+                backend_long = FINUFFTBackend(),
+                quadrature_short = GaussLegendreQuadrature(4),
+                quadrature_long = GaussLegendreQuadrature(4),
+            )
+        end
 
-        # Check that the reconnection happened at the right place
-        i = length(f) ÷ 4
-        j = 3i
-        @test length(f1) == length(f2) == length(f) ÷ 2
-        @test f1 == f[i + 1:j]
-        @test f2 == vcat(f[begin:i], f[j + 1:end])
+        tspan = (0.0, 1.0)  # ignored
+        prob = @inferred VortexFilamentProblem(fs, tspan, params_bs)
+        iter = @inferred init(
+            prob, RK4();
+            dt = 1.0,  # will be changed by the adaptivity
+            dtmin = 1e-4,
+            refinement = BasedOnCurvature(π / 8; ℓ_min = l_min / 2),
+            reconnect = BasedOnDistance(l_min),
+            adaptivity = BasedOnSegmentLength(1.0),
+        )
+
+        @assert length(iter.fs) == 1
+
+        for n = 1:200
+            Timestepping.step!(iter)
+            (; t,) = iter.time
+            Nf = length(iter.fs)
+
+            if t < 1.4
+                # This is completely empirical, but the first self-reconnection
+                # seems to occur "exactly" at t = 1.5 (between 1.4999 and 1.511).
+                # I guess this will depend a lot on the parameters...
+                @test Nf == 1
+            elseif 1.5 < t < 1.6
+                # Note that there may be other self-reconnections later, including
+                # tiny vortex rings which might be removed in future versions of the code...
+                @test Nf == 2
+            end
+        end
+
+        @test iter.time.t > 2.0  # should be actually closer to 3
     end
 end
 
