da28e4081e1a5d157b78dcc540f1ecf804d7b385
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-06-06 18:58:27 +0200

    Define AdaptBasedOnVelocity
    Also, allow combining multiple adaptivity criteria.

--- src/Timestepping/adaptivity.jl
+++ src/Timestepping/adaptivity.jl
@@ -1,4 +1,8 @@
-export NoAdaptivity, AdaptBasedOnSegmentLength
+export NoAdaptivity,
+       AdaptBasedOnSegmentLength,
+       AdaptBasedOnVelocity
+
+using LinearAlgebra: norm
 
 """
     AdaptivityCriterion
@@ -9,8 +13,19 @@ Implemented adaptivity criteria are:
 
 - [`NoAdaptivity`](@ref): disables time adaptivity;
 
-- [`AdaptBasedOnSegmentLength`](@ref): CFL-like condition, based on the minimum
-  distance between two filament nodes.
+- [`AdaptBasedOnSegmentLength`](@ref): determines the timestep ``Δt`` based on the minimum
+  distance ``ℓ_{\\min}`` between two filament nodes (``Δt ∝ ℓ_{\\min}^{-2}``);
+
+- [`AdaptBasedOnVelocity`](@ref): determines the timestep ``Δt`` based on the maximum velocity
+  ``v_{\\max}`` of filament nodes and on a predefined distance ``δ`` (``Δt = δ / v_{\\max}``).
+
+## Combining multiple criteria
+
+Adaptivity criteria can be combined using `|`. Example:
+
+    adaptivity = AdaptBasedOnSegmentLength(1.4) | AdaptBasedOnVelocity(0.01)
+
+As expected, the timestep ``Δt`` will be chosen so that it satisfies both criteria.
 """
 abstract type AdaptivityCriterion end
 
@@ -28,15 +43,15 @@ estimate_timestep(::NoAdaptivity, iter::AbstractSolver) = get_dt(iter)  # don't 
     AdaptBasedOnSegmentLength <: AdaptivityCriterion
     AdaptBasedOnSegmentLength(γ::Float64)
 
-Adapt timestep ``Δt`` based on the minimum distance ``ℓ`` between two filament nodes.
+Adapt timestep ``Δt`` based on the minimum distance ``ℓ_{\\min}`` between two filament nodes.
 
-More precisely, the timestep is set to ``Δt = γ ℓ² / β(ℓ)``, where ``γ`` is a
-dimensionless factor to be chosen. Here ``β(ℓ) = Γ / 4π \\left[ \\ln(2ℓ / a) - Δ
+More precisely, the timestep is set to ``Δt = γ ℓ_{\\min}² / β(ℓ_{\\min})``, where ``γ`` is a
+dimensionless factor to be chosen. Here ``β(ℓ) = Γ / 4π \\left[ \\ln(2ℓ_{\\min} / a) - Δ
 \\right]`` (see [`ParamsBiotSavart`](@ref) for the definitions of ``Γ``, ``a``
 and ``Δ``).
 
 Note that this criterion is based on simple dimensional analysis, assuming that
-``Δt`` must only depend on the minimum segment length ``ℓ`` and on the vortex
+``Δt`` must only depend on the minimum segment length ``ℓ_{\\min}`` and on the vortex
 circulation ``Γ`` (or rather on ``β``, which is proportional to ``Γ`` and also
 includes the effect of the vortex core size and profile).
 
@@ -83,4 +98,48 @@ function estimate_timestep(crit::AdaptBasedOnSegmentLength, iter::AbstractSolver
     dt = γ * ℓ_min^2 / β
 
     dt
+end
+
+"""
+    AdaptBasedOnVelocity <: AdaptivityCriterion
+    AdaptBasedOnVelocity(δ::Float64)
+
+Adapt timestep ``Δt`` based on the maximum velocity ``v_{\\max}`` of filament nodes and on
+the distance ``δ`.
+
+The timestep is set to ``Δt = δ / v_{\\max}``.
+
+One application of this criterion is to ensure that reconnections happen in-between two
+solver iterations. In this case, ``δ`` should be proportional to the chosen distance below
+which reconnections are performed.
+"""
+struct AdaptBasedOnVelocity <: AdaptivityCriterion
+    δ :: Float64
+end
+
+function estimate_timestep(crit::AdaptBasedOnVelocity, iter::AbstractSolver)
+    (; δ,) = crit
+    (; vs,)  = iter
+    vmax = maximum(vs) do vnodes
+        maximum(norm, vnodes)  # maximum velocity norm among the nodes of a single filament
+    end
+    δ / vmax
+end
+
+struct CombinedAdaptivityCriteria{
+        Criteria <: Tuple{Vararg{AdaptivityCriterion}}
+    } <: AdaptivityCriterion
+    criteria :: Criteria
+end
+
+CombinedAdaptivityCriteria(args...) = CombinedAdaptivityCriteria(args)
+
+Base.:|(a::AdaptivityCriterion, b::AdaptivityCriterion) =
+    CombinedAdaptivityCriteria(a, b)
+Base.:|(a::CombinedAdaptivityCriteria, b::AdaptivityCriterion) =
+    CombinedAdaptivityCriteria(a.criteria..., b)
+
+function estimate_timestep(crit::CombinedAdaptivityCriteria, iter::AbstractSolver)
+    (; criteria,) = crit
+    minimum(c -> estimate_timestep(c, iter), criteria)  # choose the smallest estimated timestep
 end
