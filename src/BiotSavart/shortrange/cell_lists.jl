0465bfac7f047ba4a19b2c5de27e60522d56a229
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-06-28 11:45:48 +0200

    Implement cell lists using ghost cells
    Can be much faster than before!

--- src/BiotSavart/shortrange/cell_lists.jl
+++ src/BiotSavart/shortrange/cell_lists.jl
@@ -1,5 +1,6 @@
 export CellListsBackend
 
+using ..Filaments: PaddedArray
 using StaticArrays: StaticArrays, similar_type, Size
 using Static: StaticInt, static, dynamic
 
@@ -35,13 +36,14 @@ struct SegmentCellList{
         N,  # usually N = 3 (=> 3D space)
         S <: Segment,
         SegmentList <: AbstractVector{S},
+        M,  # number of cell subdivisions (≥ 1)
+        CellList <: PaddedArray{M, SegmentList, N},  # array of cells, each containing a list of segments
         CutoffRadii <: NTuple{N, Real},
-        SubDiv <: StaticInt,
         Periods <: NTuple{N, Real},
     }
-    segments :: Array{SegmentList, N}  # segments[i, j, k] contains all filament segments inside cell (i, j, k)
-    rs_cut   :: CutoffRadii            # cutoff radii (can be different in each direction)
-    nsubdiv  :: SubDiv
+    segments :: CellList       # segments[i, j, k] contains all filament segments inside cell (i, j, k)
+    rs_cut   :: CutoffRadii    # cutoff radii (can be different in each direction)
+    nsubdiv  :: StaticInt{M}
     Ls       :: Periods
 end
 
@@ -55,8 +57,8 @@ function SegmentCellList(
         "infinite non-periodic domains not currently supported by CellListsBackend"
     ))
 
-    ndiv = dynamic(nsubdiv)
-    rs_cut = map(r -> r / ndiv, rs_cut_in)
+    M = dynamic(nsubdiv)
+    rs_cut = map(r -> r / M, rs_cut_in)
 
     # Number of cells in each direction.
     # Using `floor` below means that, if `rcut` doesn't exactly divide the domain size L in
@@ -65,17 +67,32 @@ function SegmentCellList(
         floor(Int, L / rcut)
     end
 
+    all(≥(2M), ncells) || error(
+        lazy"""number of cells $ncells is too small for periodic padding.
+               Try reducing the cutoff radius (got rs_cut = $rs_cut_in)."""
+    )
+
     S = Segment{Filament}
     @assert isconcretetype(S)
 
     vempty = Vector{S}(undef, 0)
     # vempty = StructVector{S}(undef, 0)  # not necessarily faster than a regular Vector in this case
     SegmentList = typeof(vempty)
-    segs = Array{SegmentList, N}(undef, ncells)
+    data_dims = ncells .+ 2M  # add 2M ghost cells in each direction
+    data = Array{SegmentList, N}(undef, data_dims)
 
-    for i ∈ eachindex(segs)
-        segs[i] = copy(vempty)
+    segs = PaddedArray{M}(data)
+    @assert size(segs) == ncells
+
+    # Initialise cells inside the domain (i.e. not including ghost cells)
+    for I ∈ CartesianIndices(segs)
+        segs[I] = copy(vempty)
     end
+
+    # Pad array periodically. Note that this needs to be done only once (and not whenever
+    # filaments are added), since we're copying array references ("pointers"), so modifying
+    # a "central" cell will also modify its corresponding ghost cell if it has one.
+    Filaments.pad_periodic!(segs)
 
     SegmentCellList(segs, rs_cut, nsubdiv, Ls)
 end
@@ -189,32 +206,14 @@ struct CellListSegmentIterator{
     end
 end
 
-# Periodic wrapping of array indices
-function _wrap_index(inds::AbstractRange, i::Int)
-    while i < first(inds)
-        i += length(inds)
-    end
-    while i > last(inds)
-        i -= length(inds)
-    end
-    i
-end
-
-_wrap_index(A::AbstractArray{T, N} where {T}, I::NTuple{N}) where {N} =
-    map(_wrap_index, axes(A), I)
-
-_wrap_index(A::AbstractArray, I::CartesianIndex) = _wrap_index(A, Tuple(I))
-
 # As of Julia 1.9.1, the @inline is needed to avoid poor performance.
-# This seems to be related to the use of Iterators.product (type of `cell_indices`)...
 @inline function Base.iterate(it::CellListSegmentIterator, state = nothing)
     (; cl, cell_indices,) = it
     (; segments,) = cl
 
     if state === nothing  # initial iteration
         cell_index, cell_indices_state = iterate(cell_indices)
-        I = _wrap_index(segments, cell_index)
-        @inbounds segments_in_current_cell = segments[I...]
+        @inbounds segments_in_current_cell = segments[cell_index]
         ret_segment = iterate(segments_in_current_cell)  # get first segment of first cell (or `nothing`, if the cell is empty)
     else
         (cell_indices_state, segments_in_current_cell, segments_state,) = state
@@ -233,8 +232,7 @@ _wrap_index(A::AbstractArray, I::CartesianIndex) = _wrap_index(A, Tuple(I))
         ret_cell = iterate(cell_indices, cell_indices_state)
         ret_cell === nothing && return nothing  # we're done iterating over cells
         cell_index, cell_indices_state = ret_cell
-        I = _wrap_index(segments, cell_index)
-        @inbounds segments_in_current_cell = segments[I...]
+        @inbounds segments_in_current_cell = segments[cell_index]
         ret_segment = iterate(segments_in_current_cell)
         cell_indices_state, segments_in_current_cell, ret_segment
     end
@@ -252,10 +250,8 @@ function nearby_segments(c::CellListsCache, x⃗::Vec3)
     inds_central = map(determine_cell_index, Tuple(x⃗), rs_cut, Ls, size(segments))
     I₀ = CartesianIndex(inds_central)  # index of central cell (where x⃗ is located)
     δ = dynamic(nsubdiv)
-    offsets = ntuple(n -> -δ - 1 + n, Val(2δ + 1))  # index offset along each dimension
-    inds_per_direction = map(Tuple(I₀)) do i
-        map(off -> i + off, offsets)
-    end
-    cell_indices = Iterators.product(inds_per_direction...)  # iterate over the M³ cells around I₀
+    cell_indices = CartesianIndices(
+        map(i -> (i - δ):(i + δ), Tuple(I₀))
+    )
     CellListSegmentIterator(cl, cell_indices)
 end
