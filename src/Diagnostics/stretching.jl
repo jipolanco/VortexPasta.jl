b2fd660d9161994a3fd3a243bd20242fff67b09e
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-07-01 13:22:48 +0200

    Use alternative stretching rate expression

--- src/Diagnostics/stretching.jl
+++ src/Diagnostics/stretching.jl
@@ -5,18 +5,19 @@ export stretching_rate
 
 Compute stretching rate of one or more vortices.
 
-The stretching rate has units ``L T^{-1}`` and is given by
+The stretching rate has units ``L T^{-1}`` and is given by:
 
 ```math
 \frac{\mathrm{d} \mathcal{L}}{\mathrm{d} t}
 = ∮ \frac{∂\bm{v}}{∂ξ} ⋅ \mathrm{d}\bm{s}
+= - ∮ \bm{v} ⋅ \bm{s}'' \, \mathrm{d}ξ
 ```
 
-where ``ξ`` is the arc length, ``\bm{v}(ξ)`` the local filament velocity, and $\mathcal{L}$
-the instantaneous vortex length.
+where ``ξ`` is the arc length, ``\bm{v}(ξ)`` the local filament velocity, ``\bm{s}''(ξ)``
+the local curvature vector, and $\mathcal{L}$ the instantaneous vortex length.
+The last equality is obtained using integration by parts.
 
-The velocity derivatives are obtained using the same interpolation method used to represent
-the filaments `fs`.
+In the implementation, the last expression is the one used to compute the stretching rate.
 
 ## Mandatory arguments
 
@@ -41,8 +42,32 @@ function stretching_rate(fs::VectorOfFilaments, vs::SetOfFilamentsData; kws...)
 end
 
 function stretching_rate(f::AbstractFilament, vs::SingleFilamentData; quad = nothing)
-    # We interpolate the filament velocities using the same knots `ts` of the filaments.
-    # This allows us to obtain the spatial derivative of the velocity `v⃗'(τ)` along the filament.
+    _stretching_rate(quad, f, vs)
+end
+
+function _stretching_rate(quad::Nothing, f::ClosedFilament, vs)
+    T = number_type(vs)
+    x = zero(T)
+    ts = Filaments.knots(f)
+    for i ∈ eachindex(f, vs)
+        v⃗ = vs[i]
+        s⃗′ = f[i, Derivative(1)]
+        s⃗″ = f[i, Derivative(2)]
+        δt = (ts[i + 1] - ts[i - 1]) / 2
+        s′³ = sqrt(sum(abs2, s⃗′))^3  # = |s⃗′|³
+        # Note: the integrand is (v⃗ ⋅ ρ⃗) * |s⃗′|, where ρ⃗ is the curvature vector and
+        # |s⃗′| = dζ/dt is the conversion from arc length ζ to arbitrary
+        # parametrisation t.
+        x += (v⃗ ⋅ (s⃗′ × (s⃗′ × s⃗″))) * (δt / s′³)
+    end
+    x
+end
+
+# Case with quadratures (requires interpolating the velocity along filaments)
+function _stretching_rate(quad, f::ClosedFilament, vs)
+    T = number_type(vs)
+    x = zero(T)
+
     method = Filaments.discretisation_method(f)
     ts = Filaments.knots(f)
     M = Filaments.npad(method)
@@ -51,53 +76,32 @@ function stretching_rate(f::AbstractFilament, vs::SingleFilamentData; quad = not
     # We use Bumper to avoid allocations managed by Julia's garbage collector.
     buf = Bumper.default_buffer()
 
-    local x
-
     @no_escape buf begin
         V = eltype(vs)
-        @assert V <: Vec3
         data = @alloc(V, Np + 2M)
         cs = PaddedVector{M}(data)
-        nderiv = max(1, Filaments.required_derivatives(method))  # we need at least 1 derivative
+        nderiv = Filaments.required_derivatives(method)
         cderiv = ntuple(Val(nderiv)) do _
             local data = @alloc(V, Np + 2M)
             PaddedVector{M}(data)
         end
-        # We interpolate the velocity v⃗, which we then derive and integrate along filaments.
+        # We interpolate the velocity v⃗, which we then integrate along filaments.
         coefs = Filaments.init_coefficients(method, cs, cderiv)
         copyto!(cs, vs)
         Filaments.compute_coefficients!(coefs, ts)
-        x = _stretching_rate(quad, f, coefs, ts)
+        for i ∈ eachindex(segments(f))
+            x += integrate(f, i, quad) do f, i, ζ
+                s⃗′ = f(i, ζ, Derivative(1))
+                s⃗″ = f(i, ζ, Derivative(2))
+                s′³ = sqrt(sum(abs2, s⃗′))^3  # = |s⃗′|³
+                v⃗ = Filaments.evaluate(coefs, ts, i, ζ, Derivative(0))
+                # Note: the integrand is (v⃗ ⋅ ρ⃗) * |s⃗′|, where ρ⃗ is the curvature vector and
+                # |s⃗′| = dζ/dt is the conversion from arc length ζ to arbitrary
+                # parametrisation t.
+                (v⃗ ⋅ (s⃗′ × (s⃗′ × s⃗″))) / s′³
+            end :: T
+        end
     end
 
-    x
-end
-
-function _stretching_rate(quad::Nothing, f::ClosedFilament, coefs, ts)
-    T = eltype(eltype(coefs))
-    @assert T <: AbstractFloat
-    x = zero(T)
-    for i ∈ eachindex(f)
-        # TODO: avoid interpolations if evaluating right on a node? (makes more sense for Hermite interpolations)
-        ζ = zero(T)
-        v⃗′ = Filaments.evaluate(coefs, ts, i, ζ, Derivative(1))  # spatial derivative (along the filament) of the filament velocity
-        t̂ = f[i, UnitTangent()]
-        δt = (ts[i + 1] - ts[i - 1]) / 2
-        x += (v⃗′ ⋅ t̂) * δt
-    end
-    x
-end
-
-function _stretching_rate(quad, f, coefs, ts)
-    T = eltype(eltype(coefs))
-    @assert T <: AbstractFloat
-    x = zero(T)
-    for i ∈ eachindex(segments(f))
-        x += integrate(f, i, quad) do f, i, ζ
-            v⃗′ = Filaments.evaluate(coefs, ts, i, ζ, Derivative(1))
-            t̂ = f(i, ζ, UnitTangent())
-            v⃗′ ⋅ t̂
-        end :: T
-    end
     x
 end
