d35daaf6084b1003e4db0de944682c5191371137
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-08-29 15:03:07 +0200

    Update docs

--- src/BiotSavart/longrange/ka_utils.jl
+++ src/BiotSavart/longrange/ka_utils.jl
@@ -6,8 +6,10 @@
 
 Get KernelAbstractions (KA) backend associated to a given long-range backend.
 
-The word "backend" means two different things here! For KA, it refers to the device where
-kernels are executed (e.g. `CPU`, `CUDABackend`, ...).
+!!! note
+
+    The word "backend" means two different things here!
+    For KA, it refers to the device where kernels are executed (e.g. `CPU`, `CUDABackend`, ...).
 
 By default this returns `KA.CPU(static = true)`, meaning that things are run on the CPU
 using threads, and that a static thread assignment is used.
