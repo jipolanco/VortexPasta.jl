efe397a8dfd409adfc75100279e12652a300e4f5
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-09-11 16:38:58 +0200

    Fix extension

--- ext/VortexPastaThreadPinningExt.jl
+++ ext/VortexPastaThreadPinningExt.jl
@@ -5,8 +5,10 @@ using ThreadPinning: ThreadPinning, pinthreads, unpinthreads
 
 # Note: we only redefine this function for FINUFFTBackend (but not for CuFINUFFTBackend,
 # which doesn't use CPU threads).
-function finufft_unpin_threads(f::F, ::FINUFFTBackend) where {F}
-    was_pinned = ThreadPinning.ispinned()  # if threads have been pinned
+# This is a workaround for apparent conflicts between OpenMP (used by FINUFFT) and
+# ThreadPinning.pinthreads.
+function BiotSavart.finufft_unpin_threads(f::F, ::FINUFFTBackend) where {F}
+    was_pinned = ThreadPinning.ispinned()   # threads are currently pinned?
     if was_pinned
         cpuids = ThreadPinning.getcpuids()  # for restoring pinning later
         ThreadPinning.unpinthreads()        # disable pinning before calling f
