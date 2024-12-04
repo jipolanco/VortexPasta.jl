6b5cb2b627740e06168e02122ccd05b65c6a3050
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-12-04 10:57:52 +0100

    Split files

---
+++ src/SyntheticFields/uniform.jl
@@ -0,0 +1,17 @@
+"""
+    UniformVectorField{T, N} <: SyntheticVectorField{T, N}
+    UniformVectorField(u⃗)
+
+Represents a uniform (constant) vector field with no spatial fluctuations.
+
+This can be used for instance to represent a uniform counterflow.
+
+The input `u⃗` can be an `SVector{N, T}` or a tuple.
+"""
+struct UniformVectorField{T, N} <: SyntheticVectorField{T, N}
+    u⃗::SVector{N, T}
+end
+
+UniformVectorField(u⃗::Tuple) = UniformVectorField(SVector(u⃗))
+
+(field::UniformVectorField)(x⃗) = field.u⃗
