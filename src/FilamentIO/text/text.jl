1cde5e09f17390e85e2a7be80b352aa20ed800ec
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2025-03-04 15:11:53 +0100

    Add missing docs

--- src/FilamentIO/text/text.jl
+++ src/FilamentIO/text/text.jl
@@ -17,6 +17,8 @@ The output is a text file where:
 
 - the end node is included. For a closed filament, this means that the first and last written points are identical.
   This also means that we write `N + 1` values for a filament of length `N` (= number of _independent_ nodes).
+
+One can use [`read_from_text`](@ref) to generate filaments from files written by this function.
 """
 function write_to_text(io::IO, fs::AbstractVector{<:AbstractVector}; delim = '\t')
     for f in fs
@@ -34,6 +36,16 @@ end
 write_to_text(filename::AbstractString, fs::AbstractVector{<:AbstractVector}; kws...) =
     open(io -> write_to_text(io, fs; kws...), filename, "w")
 
+"""
+    read_from_text(io::IO, ::Type{T}, method::DiscretisationMethod) -> fs
+    read_from_text(filename::AbstractString, ::Type{T}, method::DiscretisationMethod) -> fs
+
+Read filament locations from text file.
+
+This function can be used to read filament locations written by [`write_to_text`](@ref).
+
+The type `T` corresponds to the wanted numerical precision. It is usually `Float64` or `Float32`.
+"""
 function read_from_text(io::IO, ::Type{T}, method::DiscretisationMethod; delim = '\t') where {T}
     FilamentType = typeof(Filaments.init(ClosedFilament{T}, 0, method))
     fs = FilamentType[]
