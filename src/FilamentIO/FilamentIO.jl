c1c572caa7523f377a502e40a41644a69e2ce640
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2025-03-04 14:38:31 +0100

    Add basic text format to FilamentIO (#31)
    * Restructure FilamentIO source files
    
    * Add text format to FilamentIO
    
    * Add missing file!
    
    * Export read_from_text

--- src/FilamentIO/FilamentIO.jl
+++ src/FilamentIO/FilamentIO.jl
@@ -13,7 +13,7 @@ using ..Filaments: DiscretisationMethod, GeometricQuantity, Vec3
 # Here i ∈ 1:refinement.
 on_segment_location(i, refinement) = (i - 1) / refinement
 
-include("json_vtk_series.jl")
-include("hdf5.jl")
+include("VTKHDF/vtkhdf.jl")
+include("text/text.jl")
     
 end
