8c877715b72ee1af2b4a2e70b0a7054db640df9f
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2025-04-25 13:43:17 +0200

    Disable static threading

--- src/BiotSavart/longrange/ka_utils.jl
+++ src/BiotSavart/longrange/ka_utils.jl
@@ -11,12 +11,11 @@ Get KernelAbstractions (KA) backend associated to a given long-range backend.
     The word "backend" means two different things here!
     For KA, it refers to the device where kernels are executed (e.g. `CPU`, `CUDABackend`, ...).
 
-By default this returns `KA.CPU(static = true)`, meaning that things are run on the CPU
-using threads, and that a static thread assignment is used.
+By default this returns `KA.CPU()`, meaning that things are run on the CPU using threads.
 """
 KA.get_backend(::LongRangeBackend) = ka_default_cpu_backend()
 
-ka_default_cpu_backend() = KA.CPU(static = true)
+ka_default_cpu_backend() = KA.CPU()
 
 # Default workgroup size used for running KA kernels.
 function ka_default_workgroupsize(::KA.CPU, dims::Dims)
