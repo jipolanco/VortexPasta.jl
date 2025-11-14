5e97b84e8c87509ac44533f78d6a9b5e0a5b767d
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2025-11-14 12:22:14 +0100

    Add some basic benchmarks

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,113 @@
+# Define BenchmarkTools suite for CI
+# https://github.com/MilesCranmer/AirspeedVelocity.jl
+
+using VortexPasta
+using VortexPasta.PredefinedCurves
+using VortexPasta.Containers: VectorOfVectors
+using VortexPasta.Filaments
+using VortexPasta.BiotSavart
+using Rotations: Rotations
+using StableRNGs: StableRNG
+using BenchmarkTools
+
+using ThreadPinning
+pinthreads(:cores)
+threadinfo()
+
+function generate_biot_savart_parameters(;
+        Ls::NTuple{3, T}, β = 3.5,
+        Ns::Union{Nothing, Dims{3}} = nothing,
+        rcut::Union{Nothing, Real} = Ns === nothing ? min(Ls...) / 3 : nothing,
+    ) where {T}
+    Γ = 1.0
+    a = 1e-7
+    Δ = 1/2
+    if Ns === nothing
+        @assert rcut isa Real
+        α = β / rcut
+        kmax = 2α * β
+        Ns = ceil.(Int, (kmax / π) .* Ls) .+ 2
+    else
+        @assert rcut === nothing
+        ks_max = @. π * (Ns - 2) / Ls
+        kmax = min(ks_max...)
+        α = kmax / (2 * β)
+        rcut = β / α
+    end
+    ParamsBiotSavart(
+        T;
+        Γ, α, a, Δ, rcut, Ls, Ns,
+        backend_short = CellListsBackend(2),
+        backend_long = NonuniformFFTsBackend(CPU(); σ = T(1.5), m = HalfSupport(4)),
+        # backend_long = NonuniformFFTsBackend(CUDABackend(); σ = T(1.5), m = HalfSupport(4)),
+        quadrature = GaussLegendre(3),
+        quadrature_near_singularity = GaussLegendre(3),
+        lia_segment_fraction = 0.2,
+    )
+end
+
+abstract type VFMInitialCondition end
+
+@kwdef struct SingleRing{T} <: VFMInitialCondition
+    R::T
+end
+
+function generate_filaments(ring::SingleRing; Ls, l_res, method)
+    (; R,) = ring
+    T = eltype(Ls)
+    N = ceil(Int, 2π * R / l_res)
+    S = define_curve(Ring(); translate = Ls ./ 2, scale = R)
+    f = Filaments.init(S, ClosedFilament{T}, N, method)
+    [f]
+end
+
+@kwdef struct RandomRings{T} <: VFMInitialCondition
+    nrings::Int
+    R::T
+end
+
+function generate_filaments(rings::RandomRings; Ls, l_res, method)
+    (; nrings, R,) = rings
+    rng = StableRNG(42)
+    N = ceil(Int, 2π * R / l_res)
+    map(1:nrings) do _
+        T = eltype(Ls)
+        translate = Ls .* rand(rng, Vec3{T})
+        rotate = rand(rng, Rotations.QuatRotation)
+        S = define_curve(Ring(); scale = R, translate, rotate)
+        Filaments.init(S, ClosedFilament{T}, N, method)
+    end
+end
+
+##
+
+## Prepare vortex configuration
+
+Ls = (2π, 2π, 2π)
+l_res = Ls[1] / 128  # typical line resolution
+method = QuinticSplineMethod()
+initial_condition = RandomRings(nrings = 100, R = π / 3)
+fs = VectorOfVectors(generate_filaments(initial_condition; Ls, l_res, method))
+
+## Prepare Biot–Savart calculations
+
+Ns = (64, 64, 64)
+params = generate_biot_savart_parameters(; Ls, Ns)
+cache = BiotSavart.init_cache(params, fs)
+vs = similar(fs)
+ψs = similar(fs)
+fields = (velocity = vs, streamfunction = ψs)
+# @time BiotSavart.velocity_on_nodes!(vs, cache, fs);
+
+## Define benchmarks
+
+# Create benchmark suite for AirspeedVelocity.jl (benchmarks on github CI)
+const SUITE = BenchmarkGroup()
+
+SUITE["BiotSavart"]["velocity"] = @benchmarkable BiotSavart.velocity_on_nodes!($vs, $cache, $fs)
+SUITE["BiotSavart"]["velocity + streamfunction"] = @benchmarkable BiotSavart.compute_on_nodes!($fields, $cache, $fs)
+
+# Example interactive usage:
+# @time tune!(SUITE)
+# results = run(SUITE["BiotSavart"]["velocity"]; verbose = true, seconds = 2)                   # 65 ms (4 threads)
+# results = run(SUITE["BiotSavart"]["velocity + streamfunction"]; verbose = true, seconds = 2)  # 78 ms (4 threads)
